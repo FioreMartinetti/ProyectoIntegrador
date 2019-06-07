6e58f4abcb03b1bf674d9b3a216b323e4452ae14
Fiore Martinetti <fmartinettirivera@udesa.edu.ar>
2019-06-07 14:41:34 -0300

    Fetch con ApiKey

--- javascript.js/home.js
+++ javascript.js/home.js
@@ -1,1 +1,29 @@
+window.addEventListener("load", function(){
 
+  var api_key = "73382cd9c327a899caf9d76fe965edc2"
+  var url= "https://api.themoviedb.org/3/genre/movie/list?api_key="+api_key
+  fetch(url)
+  .then(function(response) {
+   return response.json()
+  })
+  .then(function(respuesta) {
+   console.log(respuesta); //imprimo todo lo que me devuelve el servidor
+   console.log(respuesta.genres); //imprimo el array data, que contiene 25 elementos
+   var arrayDeGeneros = respuesta.genres;
+   var genero
+   var id
+   var name
+   for (var i = 0; i < arrayDeGeneros.length; i++) {
+     genero = arrayDeGeneros[i]
+     console.log(genero);
+     id = genero.id
+     console.log(id);
+     name = genero.name
+     console.log(name);
+   }
+
+  })
+  .catch(function(error) {
+   console.log("Error: " + error);
+  })
+})
