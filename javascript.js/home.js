958b600e330d6fad794b469ec30b1ca6b7028e73
Fiore Martinetti <fmartinettirivera@udesa.edu.ar>
2019-06-20 21:54:16 -0300

    API mejor puntuadas y estrenos

--- javascript.js/home.js
+++ javascript.js/home.js
@@ -1,10 +1,23 @@
-window.addEventListener("load", function(){
-// El window.addEventListener ("load, function()") permite que la página se cargue antes de se ejecuta
+// window.addEventListener ("load, function()") permite que la página se cargue antes de que se ejecuta
 // la funcion que queremos darle.
+window.addEventListener("load", function(){
 
+  //Validación campo de busqueda al hacer enter
+  document.querySelector(".buscador").onkeypress = function(evento) {
+
+    if (evento.code == "Enter") {
+      var busqueda = document.querySelector(".buscador");
+      if (busqueda.value.length >= 3) {
+        location.href = "resultados.html?buscar=" + busqueda.value
+      }
+      else {
+        alert("El campo de búsqueda debe tener al menos 3 caracteres.")
+      }
+    }
+  }
+// Listado de géneros
   var api_key = "73382cd9c327a899caf9d76fe965edc2"
-  var url= "https://api.themoviedb.org/3/genre/movie/list?api_key="+api_key
-  // Conectamos nuestra API key con la API key de la página.
+  var url= "https://api.themoviedb.org/3/genre/movie/list?api_key="+ api_key
   fetch(url)
   .then(function(response) {
    return response.json()
@@ -31,23 +44,11 @@ window.addEventListener("load", function(){
   .catch(function(error) {
    console.log("Error: " + error);
   })
-  //Validación campo de busqueda al hacer enter
-  document.querySelector(".buscador").onkeypress = function(evento) {
 
-    if (evento.code == "Enter") {
-      var busqueda = document.querySelector(".buscador");
-      if (busqueda.value.length >= 3) {
-        location.href = "resultados.html?buscar=" + busqueda.value
-      }
-      else {
-        alert("El campo de búsqueda debe tener al menos 3 caracteres.")
-      }
-    }
-  }
-
-fetch("https://api.themoviedb.org/3/movie/popular?api_key=" + api_key)
+//Peliculas populares
+fetch("https://api.themoviedb.org/3/movie/popular?api_key=" + api_key + "&language=en-US&page=1")
 .then(function(response) {
- return response.json()
+ return response.json();
  })
 .then(function(respuesta) {
   var pelis = respuesta.results;
@@ -57,54 +58,50 @@ for (var i = 0; i < 4; i++) {
   respuesta.results.length
   console.log("respuesta" + pelis)
   var titulo = pelis[i].title
-  var url = "https://image.tmdb.org/t/p/w500/"
-  var img = pelis[i].poster_path
+  var url = 'https://image.tmdb.org/t/p/w500/'
+  var img = pelis[i].poster_path;
   var id = pelis[i].id
-
- var li;
- li = '<li>'
- li += "<button class='estrellita'> </button>
-
   .catch(function(error) {
     console.log("error" + error)
   })
-
 }
-document.querySelector("p").innerHTML = name
-document.querySelector("h2").innerHTML = name
  })
 .catch(function(error) {
  console.log("Error: " + error);
 })
-//Películas populares
 
-fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=" + api_key)
+//Películas mejor puntuadas
+fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=" + api_key + "&language=en-US&page=1")
 .then(function(response) {
  return response.json()
  })
 .then(function(respuesta) {
 console.log(respuesta);
-console.log(respuesta.destacada);
-var arrayDeDestacadas = respuesta.destacada;
-var destacada
-var id
-var name
-for (var i = 0; i < arrayDeDestacadas.length; i++) {
-  popular = arrayDeDestacadas[i]
-  console.log(popular);
-  id = popular.id
-  console.log(id);
-  name = popular.id
-  console.log(name);
+var imagen = document.querySelector(".")
+var srcImage = "https://image.tmdb.org/t/p/w500"
+for (var i = 0; i < 10; i++) {
+  imagen[i].setAttribute("src",(srcImage + respuesta.result[i].poster_path))
 }
-document.querySelector("p").innerHTML = name
-document.querySelector("h2").innerHTML = name
+var titulo = document.querySelectorAll(".")
+for (var i = 0; i < 10; i++) {
+  titulo[i].innerHTML += respuesta.results[i].title
+}
  })
 .catch(function(error) {
  console.log("Error: " + error);
 })
-//Películas destacadas
 
+//Películas próximamente
+fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=" + api_key + "&language=en-US&page=1")
+.then(function(response) {
+ return response.json()
+ })
+.then(function(respuesta) {
+console.log(respuesta);
+
+.catch(function(error) {
+ console.log("Error: " + error);
+})
 
 
 
